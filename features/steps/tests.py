f3daacb0e07b7ef06d992c76859d996a7010ae55
yonus <moraf19@student.sdu.dk>
2022-05-12 15:10:55 +0200

    Added possibility of different entities
    buzzer, light, button, sensor added

--- features/steps/tests.py
+++ features/steps/tests.py
@@ -10,8 +10,8 @@ def step_given(context, identifier : str, position):
     if(context.receiver.getActualQ() != joint_positions):
         context.controller.moveJ(joint_positions, env.get_speed(), env.get_acceleration())
 
-@then('the signal {prep} the input "{identifier}" is "{state}"')
-@given('the signal {prep} the input "{identifier}" is "{state}"')
+@then('the signal {prep} the {entity} "{identifier}" is "{state}"')
+@given('the signal {prep} the {entity} "{identifier}" is "{state}"')
 def step_given(context, identifier : str, state : str):
     input_state = (state == "ON")
     detected = False
@@ -36,6 +36,10 @@ def step_given(context, identifier : str, state : str):
     
 @then('the output "{identifier}" is {state}')
 @given('the output "{identifier}" is {state}')
+@then('the light "{identifier}" is {state}')
+@given('the light "{identifier}" is {state}')
+@then('the buzzer "{identifier}" is {state}')
+@given('the buzzer "{identifier}" is {state}')
 def step_then(context, identifier : str, state):
    signal = (state=="ON")  # True if state is ON, else False
    output = env.get_digital_output(identifier)
@@ -54,6 +58,8 @@ def step_then(context, identifier : str, state):
     
     
 @when('the output "{identifier}" {state}')
+@when('the light "{identifier}" {state}')
+@when('the buzzer "{identifier}" {state}')
 def step_when(context, identifier : str, state):
     signal = (state=="activates")  # True if state is activates, else False
     output = env.get_digital_output(identifier)
@@ -101,51 +107,3 @@ def step_then(context, identifier : str, action):
         context.gripper.open()
     else:
         context.gripper.close()
-    
-
-'''
-
-@then('the position of the robot "{identifier}" is "{position}"')
-def step_then(context, identifier : str, position):
-    pass
-
-@then('the signal of the sensor "{identifier}" is "{state}"')
-def step_then(context, identifier : str, state):
-    pass
-    ''
-    sensor_state = (state == "ON")
-
-    t_end = time.time() + 10
-    while time.time() < t_end:
-
-        input_bits  = context.reciever.getActualDigitalInputBits()
-        actual_state = env.get_digital_input_state(env.get_digital_input(identifier), input_bits)
-
-        if actual_state == sensor_state :
-            assert True
-            break
-    
-    post_input_bits  = context.reciever.getActualDigitalInputBits()
-    post_state = env.get_digital_input_state(env.get_digital_input(identifier), input_bits)
-
-    if post_state != sensor_state :        
-        assert False
-    ''
-
-@then('the output "{identifier}" is {state}')
-def step_then(context, identifier : str, state):
-    signal = (state=="ON")
-    output = env.getDigitalOutput(identifier)
-    context.io.setStandardDigitalOut(output, signal)
-
-@then('the gripper "{identifier}" is {state}')
-def step_then(context, identifier : str, state):
-    pass
-    ''
-    desired_state_open = (state=="opened")
-    if (desired_state_open):
-        context.gripper.open()
-    else:
-        context.gripper.close()
-    ''
-'''
